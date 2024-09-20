5582fc494d7b6712909bad924903349f9de1e24b
serendiipity <imane.chk@gmail.com>
2024-09-21 00:34:54 +0100

    add struct Song

---
+++ Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  Swiftly
+//
+//  Created by imane on 21/9/2024.
+//
+
+import Foundation
+
+struct Song: Identifiable {
+    var id: String
+    var title: String
+    var artist: String
+    var votes: Int
+}
