2f9b372d6006419a2235f53db60fbfdec52d7153
serendiipity <imane.chk@gmail.com>
2024-09-21 00:25:03 +0100

    add struct Room

---
+++ Room.swift
@@ -0,0 +1,15 @@
+//
+//  Room.swift
+//  Swiftly
+//
+//  Created by imane on 21/9/2024.
+//
+
+import Foundation
+
+struct Room {
+    var id: String
+    var hostUserId: String
+    var playlist: [Song]
+    var users: [String]
+}
